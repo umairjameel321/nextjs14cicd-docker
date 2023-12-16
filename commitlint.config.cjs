bf5d57f7e8bd216dd497cd7b1d262cfc3b7096d3
Umair Jameel <umairjameel321@gmail.com>
2023-12-17 02:30:31 +0500

    Added commit lint so we can add proper rules.

---
+++ commitlint.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [],
+  rules: {
+    'header-min-length': [2, 'always', 20],
+    'header-case-start-capital': [2, 'always'],
+    'header-end-period': [2, 'always'],
+  },
+  plugins: [
+    {
+      rules: {
+        'header-case-start-capital': ({ raw }) => {
+          return [
+            /^[A-Z]/.test(raw),
+            'Commit message must start with a capital letter',
+          ];
+        },
+        'header-end-period': ({ header }) => {
+          return [/\.$/.test(header), 'Commit message must end with a period'];
+        },
+      },
+    },
+  ],
+};
